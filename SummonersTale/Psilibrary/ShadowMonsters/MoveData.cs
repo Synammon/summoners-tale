736310f660e76ffd79c3488a2821d9d4024e41a2
Synammon <cynth.mcmahon@gmail.com>
2023-03-02 07:04:25 -0500

    Uploading Tutorial 11 (17)

---
+++ SummonersTale/Psilibrary/ShadowMonsters/MoveData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psilibrary.ShadowMonsters
+{
+    public enum TargetType { Self, Enemy }
+    public enum TargetAttribute 
+    { 
+        Health, 
+        Attack, 
+        Defence, 
+        SpecialAttack, 
+        SpecialDefense, 
+        Speed,
+        Accuracy 
+    }
+
+    public class MoveData
+    {
+        public string Name { get; set; }
+        public int Elements { get; set; }
+        public TargetType Target { get; set; }
+        public TargetAttribute TargetAttribute { get; set; }
+        public Point Mana { get; set; }
+        public Point Range { get; set; }
+        public int Status { get; set; }
+        public bool Hurts { get; set; }
+        public bool IsTemporary { get; set; }
+    }
+}
+
+// "Animations=System.Collections.Generic.Dictionary`2[System.String,SummonersTale.SpriteClasses.Animation]UnlockedMoves=System.Collections.Generic.List`1[SummonersTale.ShadowMonsters.Move]LockedMoves=System.Collections.Generic.List`1[SummonersTale.ShadowMonsters.Move]Name=GoblinElements=0Level=1Experience=0Health=20:20Attack=5Defence=5SpecialAttack=5SpecialDefence=5Speed=5Accuracy=100Status=0Moves=System.Collections.Generic.List`1[Psilibrary.ShadowMonsters.MoveData])"
---
+++ SummonersTale/Psilibrary/ShadowMonsters/ShadowMonsterData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psilibrary.ShadowMonsters
+{
+    public enum Element 
+    { 
+        Normal = 0, 
+        Fire = 1, 
+        Water = 2, 
+        Earth = 4, 
+        Wind = 8, 
+        Light = 16, 
+        Dark = 32 
+    }
+
+    public class ShadowMonsterData
+    {
+        public const int Normal = 0;
+        public const int Asleep = 1;
+        public const int Confused = 2;
+        public const int Poisoned = 4;
+        public const int Paralyzed = 8;
+        public const int Burn = 16;
+        public const int Frozen = 32;
+        
+        public string Name { get; set; }
+        public int Elements { get; set; }
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public Point Health { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefence { get; set; }
+        public int Speed { get; set; }
+        public int Accuracy { get; set; }
+        public int AttackMod { get; set; }
+        public int DefenceMod { get; set; }
+        public int SpecialAttackMod { get; set; }
+        public int SpecialDefenceMod { get; set; }
+        public int SpeedMod { get; set; }
+        public int AccuracyMod { get; set; }
+        public int Status { get; set; }
+        public List<MoveData> Moves { get; set; }
+    }
+}
